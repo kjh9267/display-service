b8dd8ebb4a2e8afd78b4f86f15b96b81b8eb9747
kjh9267 <intellijunho@kakao.com>
2024-09-27 11:06:10 +0900

    feat: Create Sleuth properties -refactor: Change spring boot version (sleuth)

--- src/main/java/me/jun/displayservice/core/application/dto/DisplayRequest.java
+++ src/main/java/me/jun/displayservice/core/application/dto/DisplayRequest.java
@@ -1,7 +1,7 @@
 package me.jun.displayservice.core.application.dto;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
